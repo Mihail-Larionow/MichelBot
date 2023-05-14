9b74db98e5bea4d8a3d6a5ca0f00891f3173e738
Mihail <mihail.larionow2001@gmail.com>
2023-05-14 15:39:44 +0300

    Added a configuration

--- src/main/java/com/michel/MichelBot/config/BotConfig.java
+++ src/main/java/com/michel/MichelBot/config/BotConfig.java
@@ -1,4 +1,19 @@
 package com.michel.MichelBot.config;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@Data
+@PropertySource("application.properties")
 public class BotConfig {
+
+    @Value("${bot.name}")
+    String botName;
+
+    @Value("${}")
+    String token;
+
 }
