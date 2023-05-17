36fe0d0e9e49e96149b968ee2bf7e3ab27615c0f
Mihail <mihail.larionow2001@gmail.com>
2023-05-17 21:44:40 +0300

    Added a string resource file

--- src/main/java/com/michel/MichelBot/service/TelegramBot.java
+++ src/main/java/com/michel/MichelBot/service/TelegramBot.java
@@ -1,6 +1,7 @@
 package com.michel.MichelBot.service;
 
 import com.michel.MichelBot.config.BotConfig;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -8,6 +9,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
+@PropertySource("application.properties")
 public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
